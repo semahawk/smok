21f660fe3f1f80d1614c37aee7ca06965cea4d6e
Szymon Urbaś <szymon.urbas@aol.com>
2013-07-03 18:30:58 +0200

    By Red: Don't show the Green Dragon nav.

--- lib/forest.php
+++ lib/forest.php
@@ -6,18 +6,18 @@ require_once("lib/villagenav.php");
 
 function forest($noshowmessage=false) {
 	global $session,$playermount;
-	tlschema("forest");
+	tlschema("Las");
 //	mass_module_prepare(array("forest", "validforestloc"));
-	addnav("Heal");
-	addnav("H?Healer's Hut","healer.php");
-	addnav("Fight");
-	addnav("L?Look for Something to Kill","forest.php?op=search");
+	addnav("Leczenie");
+	addnav("Chata Znachora","healer.php");
+	addnav("Walcz");
+	addnav("Szukaj Czego¶ Do Zabicia","forest.php?op=search");
 	if ($session['user']['level']>1)
-		addnav("S?Go Slumming","forest.php?op=search&type=slum");
-	addnav("T?Go Thrillseeking","forest.php?op=search&type=thrill");
+		addnav("Szwêdaj Siê","forest.php?op=search&type=slum");
+	addnav("Szukaj Przygód","forest.php?op=search&type=thrill");
 	if (getsetting("suicide", 0)) {
 		if (getsetting("suicidedk", 10) <= $session['user']['dragonkills']) {
-			addnav("*?Search `\$Suicidally`0", "forest.php?op=search&type=suicide");
+			addnav("*?Szukaj `\$Samobójczo`0", "forest.php?op=search&type=suicide");
 		}
 	}
 	if ($session['user']['level']>=15  && $session['user']['seendragon']==0){
@@ -34,18 +34,33 @@ function forest($noshowmessage=false) {
 				break;
 			}
 		}
-		if ($isforest || count($vloc)==0) {
-			addnav("G?`@Seek Out the Green Dragon","forest.php?op=dragon");
-		}
+	//	if ($isforest || count($vloc)==0) {
+	//			if ($session['user']['race'] == 'Jaszczurzyc')
+	//		addnav("G?`4Szukaj Czarnoksiê¿nika","forest.php?op=dragon");
+	//		else
+	//		addnav("G?`@Szukaj Zielonego Smoka","forest.php?op=dragon");
+	//	}
 	}
-	addnav("Other");
+	addnav("Inne");
 	villagenav();
+if (($session['user']['superuser'] & SU_MEGAUSER) || ($session['user']['login']=="Meandra")){
+	      if (is_module_active("podwodnyswiat")&&$session['user']['location'] == 'Nautileum')
+				addnav("Podwodny ¦wiat","runmodule.php?module=podwodnyswiat&op=");}
 	if ($noshowmessage!=true){
-		output("`c`7`bThe Forest`b`0`c");
-		output("The Forest, home to evil creatures and evildoers of all sorts.`n`n");
-		output("The thick foliage of the forest restricts your view to only a few yards in most places.");
-		output("The paths would be imperceptible except for your trained eye.");
-		output("You move as silently as a soft breeze across the thick moss covering the ground, wary to avoid stepping on a twig or any of the numerous pieces of bleached bone that populate the forest floor, lest you betray your presence to one of the vile beasts that wander the forest.`n");
+	if ($session['user']['location'] == 'Nautileum')
+  {
+		output("`c`#`bRafa`b`0`c");
+		output("Rafa koralowa, dom setek morskich stworzeñ.`n`n");
+		output("B³êkitny przestwór ciep³ego oceanu otacza ciê zewsz±d. Feeria barw i kszta³tów koralowców, szkar³upni, ryb i Trytonów budzi szacunek i podziw dla piêkna podwodnego ¶wiata. Jednak ta cudowna sceneria jest jednocze¶nie siedzib± najdzikszych bestii, które tylko czekaj± by zaatakowaæ nieostro¿nego podró¿nika. Zdaj±c sobie z tego doskonale sprawê, mocniej zaciskasz d³onie na swym trójzêbie i p³yniesz ku g³êbinom.`n");
+		rawoutput("<br><br><center><img src='http://s2.lotgd.pl/obrazki/rafa.gif' ></center><br>");  
+		}
+    else
+		{
+		output("`c`7`bLas`b`0`c");
+		output("Las, dom wyklêtych kreatur i wyznawców z³a wszelkiego typu.`n`n");
+		output("Grube le¶ne poszycie ogranicza pole widzenia przewa¿nie do kilku kroków. ¦cie¿ki by³yby niedostrzegalne, gdyby nie Twoje wyæwiczone oczy. Poruszasz siê cicho jak delikatny wietrzyk, przedzieraj±c siê przez grub± ¶ció³kê pokrywaj±c± ziemiê i wystrzegaj±c siê nadepniêcia na ga³±zkê albo jedn± z niezliczonej liczby bielej±cych ko¶ci, przynajmniej do chwili, gdy zdradzisz sw± obecno¶æ jednej z dzikich bestii penetruj±cych puszczê.`n");
+		rawoutput("<br><br><center><img src='http://s2.lotgd.pl/obrazki/las.gif' ></center><br>");
+    }
 		modulehook("forest-desc");
 	}
 	modulehook("forest", array());
